868ecb55147b43c4e75be94d80b65284c7597887
Alex Armstrong <armstro1@uci.edu>
2018-05-11 05:20:17 -0700

    Updated py_skeleton

--- py_script_skeleton.py
+++ py_script_skeleton.py
@@ -8,7 +8,7 @@ TODO: Description (for long scripts)
 
 Author: 
     Alex Armstrong <alarmstr@cern.ch>
-TODO: Licence:
+TODO: License:
     This script is in the public domain, free from copyrights or restrictions.
     Copyright: (C) <TODO:date>; University of California, Irvine 
 ================================================================================
@@ -22,12 +22,38 @@ def main ():
     """ Main Function """
     
     global args
+    check_args(args)
+    check_environment()
+
     print "required user input = ", args.req_input
     print "optional user input = ", args.user_input
     print_hello_world()
 
 ################################################################################
 # FUNCTIONS
+def check_args(args):
+    """ Check the input arguments are as expected """
+    if args.ifile_name and not os.path.exists(args.ifile_name):
+        print "ERROR :: Cannot find input file:", args.ifile_name
+        sys.exit()
+    if os.path.exists(args.ofile_name):
+        of = args.ofile_name
+        if not os.path.exists("OLD_%s"%of):
+            print "Renaming old output file %s -> OLD_%s"%(of, of)
+            mv_cmd = 'mv %s OLD_%s'%(of, of)
+            subprocess.call(mv_cmd, shell=True)
+        else:
+            print "WARNING :: Output file already exists: %s"%of
+            print "\tConsider deleting it or its backup (OLD_%s)"%of
+            sys.exit()
+
+def check_environment():
+    """ Check if the shell environment is setup as expected """
+    if not os.environ['USER']:
+        print "ERROR :: USER variable not set"
+        sys.exit()
+        
+
 def print_hello_world(param=''):
     """ 
     function synopsis 
@@ -48,11 +74,13 @@ if __name__ == '__main__':
         parser = argparse.ArgumentParser(
                 description=__doc__,
                 formatter_class=argparse.RawDescriptionHelpFormatter)
-        parser.add_argument('req_input', 
-                            help='required user input')
-        parser.add_argument('-i', '--user_input', 
-                            default='default value', 
-                            help='optional user input')
+        parser.add_argument('-f', '--ifile_name', 
+                            #required=True,
+                            help='Input file name')
+        parser.add_argument('-o', '--ofile_name', 
+                            default='output.txt', 
+                            help='o')
+        parser.add_argument('--dry_run')
         parser.add_argument('-v', '--verbose', 
                             action='store_true', default=False, 
                             help='verbose output')
