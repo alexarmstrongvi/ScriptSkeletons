0e9288492303d191e3e7436934cbc93c7c996ccd
Alex Armstrong <armstro1@uci.edu>
2018-05-16 09:49:54 +0200

    added bash script

---
+++ bash_script_skeleton.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+################################################################################
+# Outline came mostly from Kfir Lavi at his blog "Say what?"
+# see http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
+################################################################################
+
+# Declare globals
+RUN_DIR=$PWD
+PROG_NAME="$(basename $0)"
+PROG_DIR="$(dirname $0)"
+PROG_PATH="$(cd "$PROG_DIR"; pwd)"
+ARGS="$@"
+
+if [ $PROG_PATH != "$PWD" ]; then
+    cd $PROG_PATH
+fi
+
+################################################################################
+# Useful functions
+################################################################################
+source bash_functions.sh
+
+################################################################################
+# Usage information
+function usage() {
+	cat <<- EOF
+	############################################################################
+	#    ${PROG_NAME}
+	#
+	#    TODO: Description of program. Current skeleton loops over lines of
+	#    input file and stores them in output file
+	#
+	#
+	#    OPTIONS:
+	#       Required:
+	#       -i    input file name
+	#
+	#       Optional:
+	#       -o    output file name [default: output.txt]
+	#       -r    replace/overwrite output file if it already exists
+	#       -t    run unit test to check the program
+	#       -v    run in verbose mode
+	#       -h    show this help
+	#
+	#    Examples:
+	#       Run unit tests:
+	#       $PROG_NAME -t
+	#
+	#       Run:
+	#       $PROG_NAME -i input.txt
+	############################################################################
+	EOF
+}
+
+function print_configuration() {
+	cat <<- EOF
+	########################################################################
+	#   ${PROG_NAME} Configuration
+	#
+	#   Run cmd: ${PROG_DIR}/${PROG_NAME} $ARGS
+	#   Run from: $RUN_DIR
+	#
+	#   Input file name:  $INPUT_FILE_NAME
+	#   Output file name: $OUTPUT_FILE_NAME
+	#   Overwrite output file:  $REPLACE
+	#   Run tests:  $RUN_TESTS
+	#   Verbose:    $VERBOSE
+	#   Debug mode: $DEBUG
+	#
+	########################################################################
+	EOF
+}
+################################################################################
+# Comamand line argument parsing
+function cmdline()
+{
+    while getopts "i:o:rtvdh:" OPTION; do
+        case $OPTION in
+            i) INPUT_FILE_NAME=$OPTARG ;;
+            o) OUTPUT_FILE_NAME=$OPTARG ;;
+            r) REPLACE=true ;;
+            t) RUN_TESTS=true ;;
+            v) VERBOSE=true ;;
+            d)
+                DEBUG='-x'
+                set -x
+                ;;
+            h)
+                usage
+                exit 0
+                ;;
+            \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1;
+            ;;
+        esac
+    done
+
+    ############################################################################
+    # Set defaults
+    if is_empty $OUTPUT_FILE_NAME; then
+        OUTPUT_FILE_NAME="output.txt"
+    fi
+
+    # Check arguments if not running tests
+    if is_empty $RUN_TESTS; then
+        if is_empty ${RUN_DIR}/${INPUT_FILE_NAME}; then
+            echo "ERROR :: No input file provided"
+            exit 1
+        elif is_not_file ${RUN_DIR}/${INPUT_FILE_NAME}; then
+            echo -e "ERROR :: Unable to find $INPUT_FILE_NAME in \n\t ${RUN_DIR}"
+            exit 1
+        elif is_file $OUTPUT_FILE_NAME && is_empty $REPLACE; then
+            echo "ERROR :: $OUTPUT_FILE_NAME already exists."
+            echo "ERROR :: Either delete/rename $OUTPUT_FILE_NAME or use -r flag"
+            exit 1
+        fi
+
+        ## Print configuration
+        if is_true $VERBOSE; then
+            print_configuration
+        fi
+    fi
+}
+
+################################################################################
+# Main
+################################################################################
+function main() {
+    # Create output file
+    echo "${PROG_DIR}/${PROG_NAME} $ARGS" > $OUTPUT_FILE_NAME
+
+    # Loop over input file
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        # Skip blank lines or lines starting with #
+        if is_empty "$line" || [[ ${line:0:1} == "#" ]]; then
+            continue
+        fi
+
+        # Apply command to each line
+        cmd="echo $line"
+        $cmd >> ${RUN_DIR}/${OUTPUT_FILE_NAME}
+    done < "${RUN_DIR}/${INPUT_FILE_NAME}"
+}
+
+################################################################################
+# Run main program
+if is_true $VERBOSE; then
+    echo ">> Running $PROG_NAME"
+fi
+cmdline $ARGS
+if is_true $RUN_TESTS; then
+    source "test_$PROG_NAME"
+else
+    main
+fi
+
+# Return to original directory if there was a change
+if is_not_empty $RUN_DIR && [ "$RUN_DIR" != "$PWD" ]; then
+    cd $RUN_DIR
+fi
+if is_true $VERBOSE; then
+    echo "Output stored at ${RUN_DIR}/${OUTPUT_FILE_NAME}"
+    echo "<< Successfully ran $PROG_NAME"
+fi
+set +x
+exit 0
--- cpp_main_skeleton.cpp
+++ cpp_main_skeleton.cpp
@@ -18,20 +18,92 @@ using std::string, std::cout, std::cerr;
 ////////////////////////////////////////////////////////////////////////////////
 // Declarations
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief Print help information
-void print_usage(string exec_name, string ofile_name_df, bool flag_df) {
-    printf("============================================================\n");
-    printf(" %s\n", exec_name.c_str());
-    printf(" TODO : Brief description of program\n");
-    printf("============================================================\n");
-    printf("Required Parameters:\n");
-    printf("\tInput file name\n");
-    printf("\nOptional Parameters:\n");
-    printf("\t-o, --output      Output file name [default: %s]\n", ofile_name_df.c_str());
-    printf("\t-f, --flag        Boolean flag [default: %i]\n", flag_df);
-    printf("\t-h, --help        Print this help message\n");
-    printf("============================================================\n");
-}
+/// @brief Command line argument parser
+struct Args {
+    ////////////////////////////////////////////////////////////////////////////
+    // Initialize arguments
+    ////////////////////////////////////////////////////////////////////////////
+    string PROG_NAME;  // always required
+
+    // Required arguments
+    string ifile_name;  ///< Input file name
+
+    // Optional arguments with defaults
+    string ofile_name = "output.txt";  ///< Output file name
+    bool flag = false;  ///< Boolean flag
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Print information
+    ////////////////////////////////////////////////////////////////////////////
+    void print_usage() const {
+        printf("===========================================================\n");
+        printf(" %s\n", PROG_NAME.c_str());
+        printf(" TODO : Brief description of executable\n");
+        printf("===========================================================\n");
+        printf("Required Parameters:\n");
+        printf("\t-i, --input       Input file name\n");
+        printf("\nOptional Parameters:\n");
+        printf("\t-o, --output      Output file name\n");
+        printf("\t-f, --flag        Boolean flag\n");
+        printf("\t-h, --help        Print this help message\n");
+        printf("===========================================================\n");
+    }
+
+    void print() const {
+        printf("===========================================================\n");
+        printf(" %s Configuration\n", PROG_NAME.c_str());
+        printf("===========================================================\n");
+        printf("\tInput file name:  %s\n", ifile_name.c_str());
+        printf("\tOutput file name: %s\n", ofile_name.c_str());
+        printf("\tBoolean flag:     %i\n", flag);
+        printf("===========================================================\n");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Parser
+    ////////////////////////////////////////////////////////////////////////////
+    bool parse(int argc, char* argv[]) {
+        PROG_NAME = argv[0];
+
+        // Parse arguments
+        for (int i = 0; i< argc; ++i) {
+            // Grab arguments
+            string arg = argv[i];
+            string arg_value = argc > i+1 ? argv[i+1] : "";
+            // Skip if arg set to arg value and not arg name
+            if (arg.at(0) != '-') continue;
+
+            // Check for required arguments
+            if (arg == "-i" || arg == "--input") {
+                ifile_name = arg_value;
+            } else if (arg == "-o" || arg == "--output") {
+                ofile_name = arg_value;
+            } else if (arg == "-f" || arg == "--flag") {
+                flag = true;
+            } else if (arg == "-h" || arg == "--help") {
+                print_usage();
+                return false;
+            } else {
+                cerr << "ERROR :: Unrecognized input argument: "
+                     << arg << " -> " << arg_value << '\n';
+                print_usage();
+            }
+        }
+
+        // Check arguments
+        if (ifile_name.size() == 0) {
+            cerr << "ERROR :: No input file given\n";
+            return false;
+        } else if (ofile_name == "output.txt") {
+            cerr << "ERROR :: " << ofile_name << " is an unnacceptable name\n";
+            return false;
+        } else if (flag == false) {
+            cerr << "ERROR :: " << (flag ? "True" : "False") << " is a bad flag\n";
+            return false;
+        }
+        return true;
+    }
+} args;
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief Main function
@@ -39,94 +111,19 @@ void print_usage(string exec_name, string ofile_name_df, bool flag_df) {
 /// Run with help option (-h, --help) to see available parameters
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[]) {
-    ////////////////////////////////////////////////////////////////////////////
-    // Get command line arguments
-    // Adding arguments requires updating
-    //    -  n_required_args
-    //    -  initialization of arguments
-    //    -  print_usage signature and implementation
-    //    -  argument parser
-    //    -  arugment configuation printer
-    ////////////////////////////////////////////////////////////////////////////
-
-    string exec_name = argv[0];
-
-    // Configuration options
-    uint n_required_args = 1; ///< Number of required arguments from user
-
-    // Initialize optional arguments with defaults
-    string ofile_name;
-    string ofile_name_df = ofile_name = "output.txt";
-    bool flag;
-    bool flag_df = flag = false;
-
-    // Look for help option
-    for (int i = 0; i < argc; ++i) {
-        string opt = argv[i];
-        if (opt == "-h" || opt == "--help") {
-            print_usage(exec_name, ofile_name_df, flag_df);
-            return 0;
-        }
-    }
-
-    // Check required arguments
-    if (argc < n_required_args + 1) {
-        cerr << "ERROR :: "
-             << argc - 1 << " arguments found but "
-             << n_required_args <<" required.\n";
-        print_usage(exec_name, ofile_name_df, flag_df);
+    cout << "\n====== RUNNING " << argv[0] << " ====== \n";
+    if (args.parse(argc, argv)) {
+        // Finished parsing arguments
+        args.print();
+    } else {
+        // Failed to parse arguments or help requested
         return 1;
     }
-
-    // Initialize required arguments
-    string ifile_name = argv[1];
-
-    // Parse for optional arguments
-    for (int i = n_required_args + 1; i< argc; ++i) {
-        // Grab arguments
-        string opt = argv[i];
-        string opt_value = argc > i+1 ? argv[i+1] : "";
-        if (opt.at(0) != '-') continue;
-
-        // Set optional arguments
-        if (opt == "-o" || opt == "--output") {
-            ofile_name = opt_value;
-        } else if (opt == "-f" || opt == "--flag") {
-            flag = true;
-        } else if (opt == "-h" || opt == "--help") {
-            print_usage(exec_name, ofile_name_df, flag_df);
-            return 0;
-        } else {
-            cerr << "ERROR :: Unrecognized input argument: "
-                 << opt << " -> " << opt_value << '\n';
-        }
-    }
-
-    // Check optional arguments
-    if (ifile_name.size() == 0) {
-        cerr << "ERROR :: Input file name is blank\n";
-        return 1;
-    } else if (ofile_name == "output.txt") {
-        cerr << "ERROR :: " << ofile_name << " is an unnacceptable name\n";
-        return 1;
-    } else if (flag == false) {
-        cerr << "ERROR :: " << (flag ? "True" : "False") << " is a bad flag\n";
-        return 1;
-    }
-
-    // Print argument configuration
-    printf("============================================================\n");
-    printf(" %s Settings\n", exec_name.c_str());
-    printf("============================================================\n");
-    printf("\tInput file name:  %s\n", ifile_name.c_str());
-    printf("\tOutput file name: %s\n", ofile_name.c_str());
-    printf("\tBoolean flag:     %i\n", flag);
-    printf("============================================================\n");
-
 
     ////////////////////////////////////////////////////////////////////////////
     // Main implementation
     ////////////////////////////////////////////////////////////////////////////
-    std::cout << "Hello World\n";
+    cout << "Accessing args flag: " << args.flag << '\n';
+    cout << "\n====== SUCCESSFULLY RAN " << argv[0] << " ====== \n";
     return 0;
 }
